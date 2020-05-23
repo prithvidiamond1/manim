04c4c1149bc601d3b6842f0a0bc7b97552fdd1a6
prithvidiamond1 <prithviraj645@gmail.com>
2020-05-23 18:34:41 +0530

    changed constants.py

--- manimlib/constants.py
+++ manimlib/constants.py
@@ -116,27 +116,27 @@ NO_SCENE_MESSAGE = """
 
 # There might be other configuration than pixel shape later...
 PRODUCTION_QUALITY_CAMERA_CONFIG = {
+    "pixel_height": 2160,
+    "pixel_width": 3840,
+    "frame_rate": 60,
+}
+
+HIGH_QUALITY_CAMERA_CONFIG = {
     "pixel_height": 1440,
     "pixel_width": 2560,
     "frame_rate": 60,
 }
 
-HIGH_QUALITY_CAMERA_CONFIG = {
+MEDIUM_QUALITY_CAMERA_CONFIG = {
     "pixel_height": 1080,
     "pixel_width": 1920,
     "frame_rate": 60,
 }
 
-MEDIUM_QUALITY_CAMERA_CONFIG = {
+LOW_QUALITY_CAMERA_CONFIG = {
     "pixel_height": 720,
     "pixel_width": 1280,
     "frame_rate": 30,
-}
-
-LOW_QUALITY_CAMERA_CONFIG = {
-    "pixel_height": 480,
-    "pixel_width": 854,
-    "frame_rate": 15,
 }
 
 DEFAULT_PIXEL_HEIGHT = PRODUCTION_QUALITY_CAMERA_CONFIG["pixel_height"]
